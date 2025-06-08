4f1b8c73baf9223e6a9d9f1e011009edf44b65ec
kirankumar-medikurthy <kirankumarmedikurthy@gmail.com>
2025-06-08 21:41:32 +0530

    feat: add the product description page

--- src/components/ProductDescriptionPage/ProductDescriptionPage.component.js
+++ src/components/ProductDescriptionPage/ProductDescriptionPage.component.js
@@ -2,86 +2,66 @@ import React from "react";
 import "./ProductDescriptionPage.styles.scss";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/navigation";
-
-const images = [
-  "https://placehold.co/300x300/png",
-  "https://placehold.co/300x300/png",
-  "https://placehold.co/300x300/png",
-  "https://placehold.co/300x300/png",
-];
-
-const sizes = [35, 36, 37, 38, 39, 40, 41];
 
 const ProductDescriptionPage = () => {
+  const thumbnails = [
+    "https://placehold.co/200x200/png",
+    "https://placehold.co/200x200/png",
+    "https://placehold.co/200x200/png",
+    "https://placehold.co/200x200/png",
+  ];
+
   return (
     <div className="product-description-page">
-      <div className="product-container">
-        <div className="image-preview">
-          <div className="thumbnails">
-            {images.map((img, idx) => (
-              <img src={img} key={idx} alt="thumb" className="thumbnail" />
-            ))}
-          </div>
+      <div className="content-wrapper">
+        <div className="left-section">
           <div className="main-image">
-            <img src={images[0]} alt="product" />
+            <img src="https://placehold.co/400x400/png" alt="Main Product" />
           </div>
-        </div>
-
-        <div className="product-details">
-          <h2 className="brand">Steve Madden</h2>
-          <p className="product-name">FLORENZIA Embellished Slingback Pumps</p>
-
-          <div className="price-section">
-            <span className="sale-price">AED 259</span>
-            <span className="original-price">AED 449</span>
-            <span className="discount">-42%</span>
-          </div>
-
-          <div className="size-selector">
-            <label>Select Size:</label>
-            <Swiper spaceBetween={8} slidesPerView={6} className="size-swiper">
-              {sizes.map((size) => (
-                <SwiperSlide key={size}>
-                  <button className="size-btn">{size}</button>
+          <div className="thumbnail-slider">
+            <Swiper
+              spaceBetween={10}
+              breakpoints={{
+                0: { slidesPerView: 3 },
+                480: { slidesPerView: 4 },
+                768: { slidesPerView: 5 }
+              }}
+            >
+              {thumbnails.map((src, index) => (
+                <SwiperSlide key={index}>
+                  <div className="thumbnail-wrapper">
+                    <img src={src} alt={`Thumbnail ${index + 1}`} />
+                  </div>
                 </SwiperSlide>
               ))}
             </Swiper>
           </div>
+        </div>
 
-          <button className="add-to-bag">ADD TO BAG</button>
+        <div className="right-section">
+          <h2 className="product-title">Steve Madden FLORENZIA Embellished Slingback Pumps</h2>
 
-          <div className="availability-info">
+          <div className="product-price">
+            AED 259 <span className="original-price">AED 449</span>
+          </div>
+
+          <div className="highlights">
+            <h3>Highlights</h3>
+            <ul>
+              <li>Pointed toe slingback style</li>
+              <li>Secure ankle strap with buckle</li>
+              <li>Crystal embellished upper</li>
+              <li>Flared mid-height heel</li>
+            </ul>
+          </div>
+
+          <div className="description">
+            <h3>Description</h3>
             <p>
-              <b>263 OTHERS ARE VIEWING</b>
+              These stylish Steve Madden pumps are designed for the modern fashionista. Made with 
+              quality materials and embellished with crystals, they add elegance to any outfit.
             </p>
           </div>
-
-          <div className="product-info">
-            <h3>Also available in:</h3>
-            <div className="variant-images">
-              {images.map((img, idx) => (
-                <img src={img} key={idx} alt="variant" className="variant-img" />
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="similar-products-section">
-        <h3>Similar Products</h3>
-        <div className="similar-products">
-          {images.map((img, idx) => (
-            <div className="similar-product-card" key={idx}>
-              <img src={img} alt="similar" />
-              <p className="product-title">Product Name</p>
-              <div className="price-section">
-                <span className="sale-price">AED 299</span>
-                <span className="original-price">AED 499</span>
-                <span className="discount">-40%</span>
-              </div>
-            </div>
-          ))}
         </div>
       </div>
     </div>
